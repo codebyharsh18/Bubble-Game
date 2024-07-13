1f6594a0da681b5d0fca43f1faa465874bfa83bf
codebyharsh18 <harshbhawsar018@gmail.com>
2024-07-13 16:59:12 +0530

    commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble() {
+
+    var clutter = "";
+
+    for (var i = 1; i <= 102; i++) {
+        var rn = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble"> ${rn} </div>`;
+    }
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runtimer() {
+    var timerint = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerelem").textContent = timer;
+        }
+        else {
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
+        }
+    }, 1000);
+
+}
+
+document.querySelector("#pbtm")
+    .addEventListener("click", function (details) {
+        var clickednum = (Number(details.target.textContent));
+        if (clickednum === hitrn) {
+            increaseScore();
+            makeBubble();
+            getNewHit();
+        }
+    });
+runtimer();
+makeBubble();
+getNewHit();
+
+
+
